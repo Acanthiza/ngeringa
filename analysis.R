c5c35dc7dc080a6df5849443deca54c79648b3e6
Acanthiza <nigel.willoughby@sa.gov.au>
2024-10-15 10:43:50 +1030

    draft results sent to Jode

--- analysis.R
+++ analysis.R
@@ -1,93 +1,147 @@
 
-
+  # packages -------
+  
   library("tidyverse")
   library("readxl")
   library("fs")
   library("sf")
   library("tmap")
   library("lubridate")
-  library("raster")
-
-  dat <- dir_ls(regexp = "xlsx") %>%
-    tibble::enframe(name = NULL, value = "path") %>%
-    dplyr::filter(!grepl("~",path)) %>%
-    dplyr::mutate(sheets = map(path,readxl::excel_sheets)) %>%
-    tidyr::unnest(cols = c(sheets)) %>%
-    dplyr::filter(grepl("ALL",sheets)) %>%
-    dplyr::mutate(data = map2(path,sheets,read_excel)) %>%
-    tidyr::unnest(cols = c(data)) %>%
-    setNames(gsub("[A-Z]-| .*|\\/.*","",names(.))) %>%
-    dplyr::filter(!is.na(Latitude)) %>%
-    dplyr::mutate(date = ymd(gsub("T.*","",Date))
-                  , time = gsub(".*T","",Date)
-                  , time = gsub(":\\d{3}.*","",time)
-                  , time = hm(time)
-                  , hab = substr(Habitat,1,3)
-                  )
-  
-  datGeo <- st_as_sf(dat
-                     , coords = c("Longitude","Latitude")
-                     , crs = 4326
-                     ) %>%
-    st_transform(crs = 8059)
-  
-  tm_shape(datGeo) +
-    tm_dots() +
-    tm_facets(by = "Species")
+  library("terra")
   
   
-  #-------Sentinel data---------
+  # setup -------
+  tmap::tmap_mode("view")
   
-  siteExtent <- st_bbox(datGeo) %>%
-    st_as_sfc() %>%
-    st_buffer(500)
-  
-  # Set paths
-  dir_create(path("out","sentinel"))
-  outDir  <- path("out","sentinel","sen2r_out") # output folder
-  safeDir <- path("out","sentinel","sen2r_safe")  # folder to store downloaded SAFE
-  
-  st_write(siteExtent,path("out","sentinel","extent.geojson"))
-  
-  sentinelExtent <- path("out","sentinel","extent.geojson")
-  
-  library(sen2r)
-  
-  write_scihub_login(Sys.getenv("COPERNICUS_user")
-                     , Sys.getenv("COPERNICUS_pwd")
+  tmap::tmap_options(basemaps = c("OpenStreetMap.Mapnik"
+                                  , "Esri.WorldImagery"
+                                  )
+                     , limits = c(facets.plot = 100)
+                     , max.raster = c(plot = 1e7, view = 1e6)
+                     , max.categories = 42
                      )
   
-  out_paths_1 <- sen2r(gui = FALSE
-                       , preprocess = FALSE
-                       , list_prods = c("BOA", "TOA")
-                       , list_indices = "NDVI"
-                       , step_atmcorr = "auto"
-                       , extent = sentinelExtent
-                       , extent_name = "ngeringa"
-                       , timewindow = c(as.Date("2020-01-01"), as.Date("2020-01-31"))
-                       , path_l2a = safeDir
-                       , path_out = outDir
-                       )
+  
+  # lookups ------
+  
+  taxa <- rio::import("Ngeringa ALL survey data v3.xlsx"
+                     , which = "Species-codes"
+                     , setclass = "tibble"
+                     , col_names = TRUE
+                     )
+  
+  hab <-  rio::import("Ngeringa ALL survey data v3.xlsx"
+                     , which = "Habitat Codes"
+                     , setclass = "tibble"
+                     , col_names = FALSE
+                     ) %>%
+    dplyr::rename(hab = 1, Habitat = 2) %>%
+    dplyr::mutate(Habitat = tolower(gsub("-", "", Habitat))
+                  , hab = gsub("Remant", "Remnant", hab)
+                  )
+  
+  
+  # data -------
+  
+  dat <- rio::import("Ngeringa ALL survey data v3.xlsx"
+                     , which = "ALL DATA"
+                     , setclass = "tibble"
+                     ) %>%
+    dplyr::mutate(strCode = Species
+                  , Habitat = tolower(gsub("-", "", Habitat))
+                  ) %>%
+    dplyr::left_join(taxa) %>%
+    dplyr::left_join(hab) %>%
+    setNames(gsub("[A-Z]-| .*|\\/.*","",names(.))) %>%
+    dplyr::filter(!is.na(Lat)) %>%
+    dplyr::mutate(date = ymd(gsub("T.*","",Date))
+                  , time = gsub(".*T","",Date)
+                  , time = gsub("+10:30|010:30|009:30","",time)
+                  , time = hms(time)
+                  , year = year(date)
+                  ) %>%
+    dplyr::mutate(use_hab = gsub("\\s.*|\\-.*|\\/.*", "", hab)) %>%
+    dplyr::filter(!is.na(Count)) %>%
+    dplyr::add_count(Species, name = "spp_n") %>%
+    dplyr::add_count(use_hab, name = "hab_n")
+  
+  datGeo <- st_as_sf(dat
+                     , coords = c("Long", "Lat")
+                     , crs = 4326
+                     ) %>%
+    sf::st_transform(crs = 7845)
+  
+  tm_shape(datGeo) +
+    tm_dots(col = "Species")
+  
+  
+  mod <- rstanarm::stan_glmer(Count ~ year + (year | Species) + (1 | use_hab)
+                              , data = dat
+                              , family = stats::poisson()
+                              )
+  
   
   
   #----------Site raster----------
   
-  cellVals <- datGeo %>%
-    dplyr::mutate(cell = cellFromXY(siteRaster,as_Spatial(datGeo$geometry))) %>%
-    st_set_geometry(NULL) %>%
-    as_tibble() %>%
-    dplyr::filter(!hab %in% c("OH","HO")) %>%
-    dplyr::count(cell,hab, name = "value") %>%
-    dplyr::group_by(cell) %>%
-    dplyr::filter(value == max(value)) %>%
-    dplyr::slice(1) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(value = as.numeric(as.factor(hab)))
+  if(FALSE) {
   
-  cellRAT <- tibble(cell = 1:ncell(siteRaster)) %>%
-    dplyr::left_join(cellVals)
+    #-------Satellite data---------
   
-  siteRaster <- setValues(siteRaster,cellRAT$value)
+  siteExtent <- st_bbox(datGeo) %>%
+    st_as_sfc() %>%
+    st_buffer(50)
   
-  plot(siteRaster)  
+  ras_dir <- fs::path("H:"
+                      , "data"
+                      , "raster"
+                      , "aligned"
+                      , "sa_ibrasub_xn____0__30"
+                      )
+  
+  env_info <- envRaster::parse_env_tif(ras_dir, cube = FALSE) %>%
+    dplyr::filter(!is.na(band)) %>%
+    dplyr::mutate(name = paste0(band, "__", season))
+  
+  env <- terra::rast(env_info$path) %>%
+    terra::crop(siteExtent)
+  
+  names(env) <- env_info$name
+  
+    cell_vals <- datGeo %>%
+      dplyr::mutate(cell = terra::cellFromXY(env, sf::st_coordinates(datGeo))) %>%
+      st_set_geometry(NULL) %>%
+      as_tibble() %>%
+      dplyr::count(cell, use_hab, name = "value") %>%
+      dplyr::group_by(cell) %>%
+      dplyr::filter(value == max(value)) %>%
+      dplyr::slice(1) %>%
+      dplyr::ungroup() %>%
+      dplyr::mutate(value = as.numeric(as.factor(use_hab)))
+    
+    mod_data <- cell_vals %>%
+      dplyr::filter(!is.na(cell)
+                    , !is.na(use_hab)
+                    , !use_hab %in% c("Over", "Heard")
+                    ) %>%
+      dplyr::add_count(use_hab, name = "n") %>%
+      dplyr::filter(n > 3) %>%
+      dplyr::mutate(use_hab = forcats::fct_infreq(use_hab))
+    
+    env_data <- terra::extract(env, mod_data$cell)
+    
+    mod <- randomForest::randomForest(x = env_data
+                                      , y = mod_data$use_hab
+                                      , ntree = 3000
+                                      , sampsize = rep(min(table(mod_data$use_hab)), length(table(mod_data$use_hab)))
+                                      , mtry = 2
+                                      )
+    
+    pred <- terra::predict(env
+                           , mod
+                           )
+    
+    tm_shape(pred) + tm_raster(alpha = 0.8)
+    
+  }
   
