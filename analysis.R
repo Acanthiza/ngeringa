873c53ec55a80ef97fad84f835e9675d40ac23bb
Nigel <36317413+Acanthiza@users.noreply.github.com>
2025-03-28 17:02:03 +1030

    Successful render of report: 2025-03-28

--- analysis.R
+++ analysis.R
@@ -1,14 +1,39 @@
 
   # packages -------
   
-  library("tidyverse")
-  library("readxl")
-  library("fs")
-  library("sf")
-  library("tmap")
-  library("lubridate")
-  library("terra")
+  packages <- c("base"
+                
+                # tidyverse
+                , "dplyr"
+                , "tidyr"
+                , "purrr"
+                , "tibble"
+                , "magrittr"
+                , "ggplot2"
+                , "readxl"
+                , "fs"
+                , "lubridate"
+                , "rio"
+                
+                # mapping
+                , "sf"
+                , "tmap"
+                , "terra"
+                
+                # report
+                , "knitr"
+                , "rmarkdown"
+                , "bookdown"
+                
+                # env
+                , "envFunc"
+                )
   
+  envFunc::check_packages(packages
+                          , lib = TRUE
+                          , bib = TRUE
+                          , file = "packages.bib"
+                          )
   
   # setup -------
   tmap::tmap_mode("view")
@@ -45,7 +70,8 @@
     2019, 28354,
     2020, 28354,
     2021, 28354,
-    2023, 3857
+    2023, 3857,
+    2024, 3857
     ) %>%
     dplyr::mutate(year = factor(year, ordered = TRUE))
   
@@ -55,6 +81,9 @@
                      , which = "ALL DATA"
                      , setclass = "tibble"
                      ) %>%
+    dplyr::mutate(Species = gsub("CBK", "BKB", Species)
+                  , Species = gsub("WPG", "WPH", Species)
+                  ) |>
     dplyr::mutate(strCode = Species
                   , Habitat = tolower(gsub("-", "", Habitat))
                   ) %>%
@@ -64,22 +93,20 @@
     dplyr::filter(!is.na(Lat)) %>%
     dplyr::mutate(date = ymd(gsub("T.*","",Date))
                   , time = gsub(".*T","",Date)
-                  , time = gsub("+10:30|010:30|009:30","",time)
+                  , time = gsub("\\+10:30|010:30|009:30","",time)
                   , time = hms(time)
-                  , year = year(date)
+                  , year = lubridate::year(date)
                   , year = factor(year, ordered = TRUE)
                   ) %>%
     dplyr::mutate(use_hab = gsub("\\s.*|\\-.*|\\/.*", "", hab)) %>%
-    dplyr::filter(!is.na(Count)) %>%
-    dplyr::add_count(Species, name = "spp_n") %>%
-    dplyr::add_count(use_hab, name = "hab_n")
+    dplyr::filter(!is.na(Count))
   
   dat_sf <- dat %>%
     dplyr::mutate(old_east = East
                   , old_north = North
                   ) %>%
-    dplyr::mutate(East = dplyr::if_else(year == 2023, old_north, old_east)
-                  , North = dplyr::if_else(year == 2023, old_east, old_north)
+    dplyr::mutate(East = dplyr::if_else(year %in% c(2023, 2024), old_north, old_east)
+                  , North = dplyr::if_else(year %in% c(2023, 2024), old_east, old_north)
                   ) %>%
     tidyr::nest(data = -year) %>%
     dplyr::left_join(epsg) %>%
@@ -90,11 +117,19 @@
                                                             , crs = y
                                                             , remove = FALSE
                                                             ) %>%
-                                       sf::st_transform(crs = 7854)
+                                       sf::st_transform(crs = 3857)
                                      )
                   ) %>%
     tidyr::unnest(cols = data) %>%
     sf::st_sf()
+  
+  
+  if(FALSE) {
+    
+    tm_shape(dat_sf) + tm_dots(col = "year")
+    
+    
+  }
   
   
   # Area -------
@@ -106,22 +141,57 @@
                   , hec = as.numeric(hec / 10000)
                   )
   
-  tm_shape(hec) + tm_polygons(col = "year", alpha = 0.5)
+  tm_shape(hec) + tm_polygons(alpha = 0.5)
   
-  # naive occ -----
+  # raster ------
   
-  occ <- dat %>%
+  r <- terra::rast(crs = paste0("epsg:", 3857)
+                   , resolution = c(50, 50)
+                   , extent = terra::ext(hec)
+                   )
+  
+  r_poly <- r |>
+    terra::as.polygons() |>
+    sf::st_as_sf()
+  
+  tm_shape(r_poly) +
+    tm_polygons(alpha = 0.03) +
+  tm_shape(dat_sf) +
+    tm_dots(col = "year")
+  
+  naive_occ <- dat_sf |>
+    tidyr::nest(data = -c(Species, year)) |>
+    dplyr::mutate(r = purrr::map(data
+                                  , \(x) terra::rasterize(x = terra::vect(x)
+                                                          , y = r
+                                                          , field = "Count"
+                                                          , fun = \(x) sum(x) > 0
+                                                          )
+                                  )
+                  , cells = purrr::map_dbl(r, \(x) as.numeric(terra::global(x, "sum", na.rm = TRUE)))
+                  )
+  
+  tm_shape(r_poly) +
+    tm_borders() +
+  tm_shape(naive_occ$r[[1]]) +
+    tm_raster() +
+  tm_shape(dat_sf |>
+             dplyr::filter(Species == naive_occ$Species[[1]])
+           ) +
+    tm_dots(col = "year")
+  
+  
+  # JAG 24/02/2025
+  # PUT IN 1 HA GRID and do p/a within grid
+  
+  # total count -----
+  
+  total_count <- dat %>%
     dplyr::group_by(year, Species, strCommonName) %>%
     dplyr::summarise(occ = sum(Count)) %>%
     dplyr::ungroup() %>%
-    dplyr::cross_join(hec %>%
-                       sf::st_set_geometry(NULL)
-                     ) %>%
-    dplyr::mutate(occ = occ / hec) %>%
     tidyr::pivot_wider(values_from = "occ", names_from = "year")
   
-  
-  rio::export(occ, "naive_occ.csv")
   
   # maps ------
   tmap_mode("view")
@@ -152,4 +222,40 @@
   tmap_mode("plot")
   
   tmap::tmap_save(facets, filename = "facets.png")
+  
+  
+  # year by count -------
+  
+  dat |>
+    dplyr::filter(hab != "oh") |>
+    dplyr::group_by(strCommonName , year) |>
+    dplyr::summarise(n = sum(Count)) |>
+    dplyr::ungroup() |>
+    ggplot(aes(year, n)) +
+    geom_point() +
+    geom_smooth() +
+    facet_wrap(~ strCommonName)
+  
+  # year by cell -------
+  
+  naive_occ |>
+    dplyr::left_join(taxa
+                     , by = c("Species" = "strCode")
+                     ) |>
+    ggplot(aes(year, cells)) +
+    geom_point() +
+    geom_smooth() +
+    facet_wrap(~ strCommonName)
+  
+  # report -------
+  # Clean up previous knits
+  
+  fs::file_delete(fs::dir_ls(regexp = "_book|_main"))
+  
+  bookdown::render_book(output_dir = "docs")
+  
+  envFunc::git_commit_env(paste0("Successful render of report: "
+                                 , format(Sys.Date(), "%Y-%m-%d")
+                                 )
+                          )
   
